dde455c30d4aea4c0bc8e5fd1a6cc1cfefac3934
Manuel Gorman <manuelgorman@hotmail.com>
2014-08-19 20:48:50 +0100

    Fixed JSON output
    Also tidied some things and added more comments

--- hours.py
+++ hours.py
@@ -6,7 +6,7 @@ import MySQLdb
 import json
 import sys
 
-import cgitb
+import cgitb	#Turn on fancy debugging so that if it fucks up, you know how
 cgitb.enable()
 
 arguments = cgi.FieldStorage()	#Store HTTP GET arguments in 'arguments' variable
@@ -19,6 +19,7 @@ db = "work"
 query = "SELECT * FROM hours LIMIT %s" % (arguments['results'].value)
 
 class Shift():
+	"docstring"		#I know I'm a bad person
 	def __init__(self,date,start,end,susan): #Acquire the values for an arbitrary shift
 		self.date = date
 		self.rawStart = start
@@ -35,16 +36,15 @@ class Shift():
 	def convToTime(self,timedeltaObj):		#Do some fancy stuff I found on StackOverflow to get a datetime.time object from a datetime.timedelta object
 		dt = datetime.datetime
 		return (dt.min + timedeltaObj).time()
-	def dictionary(self):	#Returns a dictionary of the values for any given instance
+	def dictionary(self):	#Returns a formatted dictionary of the values for the instance
 		dict = {}
-		dict["date"] = self.date
-		dict["start"] = self.start
-		dict["duration"] = self.duration
-		dict["end"] = self.end
-		dict["susan"] = self.susan
-		dict["earned"] = self.earned
-		return dict
-
+		dict["date"] = self.date.strftime("%a %d %b %Y")					#Format to "[Short weekday] [Day of month] [Month] [Year]"
+		dict["start"] = self.start.strftime("%I:%M %p")						#Format to "[12 hour time]:[Minute] [AM or PM]
+		dict["duration"] = self.convToTime(self.duration).strftime("%H:%M")	#Format to "[24 hour time]:[Minute]" okay its kind of hacky because its a duration but who cares, i'm the only one that's going to use this anyway
+		dict["end"] = self.end.strftime("%I:%M %p")							#Format to "[12 hour time]:[Minute] [AM or PM]
+		dict["susan"] = self.susan											#Send a good ol' boolean
+		dict["earned"] = chr(163) + str(self.earned)						#Add a £ symbol to the start of the earned number
+		return dict															#Send the dictionary on its merry way
 
 class Table:
 		newTable = "<table class=" + chr(34) + "hover" + chr(34) + ">"
@@ -58,32 +58,31 @@ class Table:
 #Set up database connection and cursor
 dbCon = MySQLdb.connect(host,uname,pword,db)
 cursor = dbCon.cursor(MySQLdb.cursors.DictCursor)
-sys.stderr.write("Database connection set up")
-
-#Prepare empty list to be filled with shift objects
-shifts = []
 
 #Run the query and store results in 'rawShifts'
 cursor.execute(query)
 rawShifts = cursor.fetchall()
 
-sys.stderr.write("Results obtained!")
+#Prepare empty list to be filled with shift objects
+shifts = []
 
-#Iterate through results and append each shift object to the list I CALLED TOO MANY THINGS SHIFT OH GOD
+#Iterate through results making an instance of the Shift object and append each instance of the Shift object to the list of shifts I CALLED TOO MANY THINGS SHIFT OH GOD
 for shift in rawShifts:
 	shiftInstance = Shift(shift['date'],shift['start'],shift['end'],shift['susan'])
 	shifts.append(shiftInstance)	#Append the object to the list
 
-if str(arguments["type"].value).upper == "TABLE":	#Send a table in HTML if requested
-	#generateTable(shifts)
+requestedOutput = str(arguments["type"].value).upper()	#Just being tidy!
+	
+if requestedOutput == "TABLE":	#Send a table in HTML if requested
+	#generateTable(shifts)	I'll get to this bit later
 	pass
-elif str(arguments["type"].value).upper() == "JSON":
+	
+elif requestedOutput == "JSON":
 	JSONShiftList = []
 	for shift in shifts:
 		JSONShiftList.append(shift.dictionary())
 	sys.stderr.write(str(JSONShiftList))
-	print "Content-Type: text/plain"	# Plain text is following (actually JSON but they don't need to know that)
-	print								# blank line, end of headers
-	print json.dumps(JSONShiftList, default=dthandler)
-	
-	
+	print "Content-Type: text/plain"	#Plain text is following (actually JSON but they don't need to know that)
+	print								#Blank line, end of headers
+	print json.dumps(JSONShiftList)		#Excrete what is hopefully JSON to whoever wants it
+
