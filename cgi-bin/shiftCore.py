a5993b75ba2dcdad0f7fea9cb5c4418d1133a457
Manuel Gorman <manuelgorman@hotmail.com>
2014-08-22 14:36:33 +0100

    Removed remainder of obsolete files
    Die, pathetic attempts at Python

--- cgi-bin/shiftCore.py
+++
@@ -1,9 +0,0 @@
-class shiftCore:
-	class table:
-		newTable = "<table class=" + chr(34) + "hover" + chr(34) + ">"
-		endTable = "</table>"
-		newRow = "<tr>"
-		newHeadRow = "<tr id=bottom>"
-		endRow = "</tr>"
-		newCol = "<td>"
-		endCol = "</td>"
